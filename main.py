e1bb092520df46d12953cbdb8dfe59a045708015
Arqam Javaid Khan <arqam60@gmail.com>
2024-04-28 20:08:29 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {'Data':{'Name': 'Arqam'}};
+
+@app.get("/about")
+def about():
+    return {'About Page'}
+                    
